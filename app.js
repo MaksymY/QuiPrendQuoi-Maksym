94963a5c203045af7405cca98245bf7771e18ed3
MaksymY <maksym.yankivskyypro@gmail.com>
2020-03-17 16:42:06 +0100

    structure is ready

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const dotenv = require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const dotenv = require('dotenv').config()
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Qui prend quoi ?' });
+	res.render('index', { title: 'Qui prend quoi ?' });
+	res.render('party', { titel: 'Party' })
 });
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+
+app.post('/party', function(req, res) {
+	axios
+		.post(`${process.env.API_URL}/party`, req.body)
+		.then(({ data }) => res.redirect(`/party/${data._id}`))
+		.catch((err) => console.error(err))
+});
+
+app.get('/party/:id', function(req, res){
+	axios
+		.get(`${process.env.API_URL}/party/${req.params.id}`)
+		.then(({ data }) =>
+			res.render('party', {
+				party: data,
+				title: data.name,
+				url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+			}),
+  )
+  .catch((err) => console.log(err));
+})
+
+app.listen(process.env.PORT, () => 
+	console.log(`Front app listening on port ${process.env.PORT}!`),
+);
